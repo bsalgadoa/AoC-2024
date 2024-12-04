3f4249cf46a86d98356a62ea79e90f38e57d0a6e
bsalgadoa <b.salgado.a@gmail.com>
2024-12-04 13:08:38 +0000

    Refactor: Update Day0 to match interface changes and add Javadoc

--- src/main/java/day0/Day0.java
+++ src/main/java/day0/Day0.java
@@ -7,10 +7,18 @@ import java.io.IOException;
 
 import aoc.DayChallenge;
 
+/**
+ * A classe Day0 é uma implementação de teste básico para assegurar que o main e a interface DayChallenge estão a funcionar.
+ *
+ * O objetivo desta classe é apenas validar a interação entre a implementação da interface e o processo de leitura de ficheiros,
+ * além de garantir que os métodos da interface sejam chamados corretamente.
+ * A parte 1 conta o número de palavras no ficheiro de entrada.
+ * A parte 2 não foi implementada.
+ */
 public class Day0 implements DayChallenge {
 
     @Override
-    public void processInput(final File inputFile) {
+    public Integer solvePart1(final File inputFile) {
         int wordCount = 0;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
@@ -22,10 +30,15 @@ public class Day0 implements DayChallenge {
                 wordCount += words.length;
             }
 
-            System.out.println("Total de palavras '" + inputFile.getName() + "': " + wordCount);
-
         } catch (final IOException e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
         }
+        return wordCount;
+    }
+
+    @Override
+    public Integer solvePart2(final File inputFile) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
